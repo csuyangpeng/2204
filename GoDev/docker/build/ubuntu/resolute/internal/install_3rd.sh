1e23273c5505ef3f73050e8cf827c4ea2ea2e050
peng.yang <peng.yang@kingsignal.com>
2026-06-22 13:55:31 +0800

    resolute GoDev

--- GoDev/docker/build/ubuntu/resolute/internal/install_3rd.sh
+++ GoDev/docker/build/ubuntu/resolute/internal/install_3rd.sh
@@ -6,6 +6,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 wget https://github.com/ntop/nDPI/archive/refs/tags/4.8.tar.gz
 tar -xf 4.8.tar.gz
-pushd nDPI-4.8 && ./autogen.sh && make && make install && popd
+pushd nDPI-4.8
+./autogen.sh
+# Dockerfile 中 MAKEFLAGS=-j 会无限制并行，buildx 下易触发 gcc 段错误
+unset MAKEFLAGS
+make -j2
+make install
+popd
 
 rm 4.8.tar.gz nDPI-4.8 -rf
--- GoDev/docker/build/ubuntu/resolute/internal/install_tcpdump.sh
+++ GoDev/docker/build/ubuntu/resolute/internal/install_tcpdump.sh
@@ -6,7 +6,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 git clone http://${GITUSER}:${GITPASS}@10.18.1.2:9999/plt/tcpdump.git
 pushd tcpdump
-mkdir build && cd build && cmake .. && make -j && make install && cd -
+mkdir build && cd build
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ..
+make -j2
+make install
+cd -
 popd
 
 rm tcpdump -rf

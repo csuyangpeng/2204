a31ff4e62c230ef1b7cf8911c108c205aad3747f
peng.yang <peng.yang@kingsignal.com>
2026-06-22 15:07:02 +0800

    GoDev

--- GoDev/docker/build/ubuntu/resolute/docker_build.sh
+++ GoDev/docker/build/ubuntu/resolute/docker_build.sh
@@ -8,11 +8,11 @@ REPOSITORY=10.18.1.2:5000
 ALIREPOSITORY=registry.cn-hangzhou.aliyuncs.com/10_18_1_2_5000
 # TAGS=dev-22.04-`date +%Y%m%d`
 TAGS=dev-26.04-`date +%Y%m%d`
-# docker build --progress=plain --add-host kafka:127.0.0.1 -t lite5gc:${TAGS} . -f Dockerfile
+docker build --progress=plain -t lite5gc:${TAGS} . -f Dockerfile
 # docker tag lite5gc:${TAGS} ${REPOSITORY}/lite5gc:${TAGS}
 # docker push ${REPOSITORY}/lite5gc:${TAGS}
-# docker tag lite5gc:${TAGS} ${ALIREPOSITORY}/lite5gc:${TAGS}
-# docker push ${ALIREPOSITORY}/lite5gc:${TAGS}
+docker tag lite5gc:${TAGS} ${ALIREPOSITORY}/lite5gc:${TAGS}
+docker push ${ALIREPOSITORY}/lite5gc:${TAGS}
 
-docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -f Dockerfile -t ${ALIREPOSITORY}/lite5gc:${TAGS} --push .
+# docker buildx build --progress=plain --platform linux/amd64,linux/arm64 -f Dockerfile -t ${ALIREPOSITORY}/lite5gc:${TAGS} --push .
 # docker buildx build --progress=plain --platform linux/amd64 -f Dockerfile -t ${ALIREPOSITORY}/lite5gc:${TAGS} --push .
